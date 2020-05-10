b866e8c401dc94335bccd04aae2c9de45c739ff5
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-10 00:18:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else 
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
