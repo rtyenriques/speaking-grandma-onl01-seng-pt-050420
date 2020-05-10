843b4356ee69282c503f19c0aedd1c50c23f7e80
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-10 00:04:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,3 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!
-def speak_to_grandma(phrase.upcase="huh?! speak up, sonny!".upcase)
-if speak_to_grandma == ".upcase"
-  puts "no, not since 1938".upcase
-elsif speak_to_grandma ==
